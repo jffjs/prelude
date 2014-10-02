9488aaf52689c0b093651364cf356221c78b0101
jffjs <jffreyjs@gmail.com>
2014-10-02 15:34:07 -0400

    porting bindings from vim

--- personal/evil.el
+++ personal/evil.el
@@ -1,8 +1,20 @@
+(setq evil-shift-width 2)
+;; Set up NERD commenter
+(global-set-key (kbd "s-;") 'evilnc-comment-or-uncomment-lines)
+
 (define-key global-map (kbd "C-6") 'prelude-switch-to-previous-buffer)
+
 (eval-after-load "evil"
 '(progn
-   (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-   (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-   (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-   (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
-   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)))
+    ;; Set up better window navigation
+    (define-key evil-normal-state-map (kbd "s-h") 'evil-window-left)
+    (define-key evil-normal-state-map (kbd "s-j") 'evil-window-down)
+    (define-key evil-normal-state-map (kbd "s-k") 'evil-window-up)
+    (define-key evil-normal-state-map (kbd "s-l") 'evil-window-right)
+    ;; Map alternate Esc
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+    ;; shifting
+    (define-key evil-normal-state-map (kbd "s-]") 'evil-shift-right-line)
+    (define-key evil-visual-state-map (kbd "s-]") 'evil-shift-right)
+    (define-key evil-normal-state-map (kbd "s-[") 'evil-shift-left-line)
+    (define-key evil-visual-state-map (kbd "s-[") 'evil-shift-left)))
--- personal/personal.el
+++ personal/personal.el
@@ -1,9 +1,11 @@
-(prelude-require-packages '(solarized-theme paredit))
+(prelude-require-packages '(solarized-theme paredit evil-nerd-commenter))
 
 (load-theme 'solarized-light t)
 (set-default-font "Inconsolata-16")
+(setq tab-width 2)
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
+(define-key global-map (kbd "s-t") 'helm-projectile)
 (key-chord-define-global "jj" nil)
 (key-chord-define-global "jl" nil)
 (key-chord-define-global "jk" nil)
